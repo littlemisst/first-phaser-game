cf1a88838bb288371550402358f62a5403c1e193
teofy <teofyrbns@gmail.com>
2020-02-11 03:34:12 +0800

    added collision to the food with score on console

--- scenes/Entity.js
+++ scenes/Entity.js
@@ -1,5 +1,4 @@
-let food, score = 0;
-console.log(score)
+let food;
 class Entity extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, key, type) {
     super(scene, x, y, key);
@@ -9,17 +8,18 @@ class Entity extends Phaser.GameObjects.Sprite {
     this.setData("type", type);
   }
 }
-const villagers = ["villager1", "villager2"];
+const villagerType = ["villager1", "villager2"];
 const types = ["villagerOne", "villagerTwo"];
 const orders = ["potatoKwekKwek", "kamoteLumpia"];
 
 class VillagerFromLeft extends Entity {
   constructor(scene, x, y) {
     const index = Phaser.Math.Between(0, 1);
-    super(scene, x, y, villagers[index], types[index]);
-    this.body.setVelocityX(Phaser.Math.Between(50, 100));
-    this.anims.play(villagers[index]);
+    super(scene, x, y, villagerType[index], types[index]);
+    this.body.setVelocityX(Phaser.Math.Between(20, 40));
+    this.anims.play(villagerType[index]);
     food = new FoodOrder(this.scene, this.x, this.y - 60, this.body.velocity.x);
+    foodOrders.add(food);
     food.setScale(0.5, 0.5).setDepth(1);
   }
 }
@@ -27,43 +27,55 @@ class VillagerFromLeft extends Entity {
 class VillagerFromRight extends Entity {
   constructor(scene, x, y) {
     const index = Phaser.Math.Between(0, 1);
-    super(scene, x, y, villagers[index], types[index]);
-    this.body.setVelocityX(Phaser.Math.Between(-50, -100));
-    this.anims.play(villagers[index]);
+    super(scene, x, y, villagerType[index], types[index]);
+    this.body.setVelocityX(Phaser.Math.Between(-30, -50));
+    this.anims.play(villagerType[index]);
     food = new FoodOrder(this.scene, this.x, this.y - 60, this.body.velocity.x);
+    foodOrders.add(food);
     food.setScale(0.5, 0.5).setDepth(1);
   }
 }
+
 class FoodOrder extends Entity {
   constructor(scene, x, y, velocity) {
-    super(scene, x, y, orders[Phaser.Math.Between(0, 1)]);
+    const order = orders[Phaser.Math.Between(0, 1)];
+    super(scene, x, y, order);
     this.body.setVelocityX(velocity);
-    scene.add.existing(this);
-    this.setInteractive();
-    scene.input.setDraggable(this, true);
-
-    scene.input.on("dragstart", function(pointer, obj) {
-      obj.body.setVelocityX(0);
-    });
-    scene.input.on("drag", function(pointer, obj, dragX, dragY) {
-      obj.setPosition(dragX, dragY);
-    });
-    scene.input.on("drop", function(pointer, obj, dropZone) {
-      score += 10
-      obj.x = dropZone.x;
-      obj.y = dropZone.y;
-      obj.destroy()
-    });
-
-    scene.time.delayedCall(5000, function (obj) {
-      this.destroy()
-    }, [], this)
+    this.setInteractive({dropZone: true}).setName(order+'Menu')
+  
+    scene.time.delayedCall(
+      5000,
+      function(obj) {
+        this.destroy();
+      },
+      [],
+      this
+    );
   }
 }
 
 class Angry extends Entity {
   constructor(scene, x, y, velocity) {
-    super(scene, x, y, 'angry');
+    super(scene, x, y, "angry");
     this.body.setVelocityX(velocity);
   }
 }
+
+
+
+// scene.input.setDraggable(this, true);
+
+//     scene.input.on("dragstart", function(pointer, obj) {
+//       obj.body.setVelocityX(0);
+//     });
+//     scene.input.on("drag", function(pointer, obj, dragX, dragY) {
+//       obj.setPosition(dragX, dragY);
+//     });
+//     scene.input.on("drop", function(pointer, obj, dropZone) {
+//       obj.x = dropZone.x;
+//       obj.y = dropZone.y;
+//       obj.input.enabled = false;
+//       score += 10
+//       console.log(score)
+//       // obj.destroy();
+//     });
--- scenes/MainGame.js
+++ scenes/MainGame.js
@@ -1,8 +1,8 @@
+let character, foodOrders, villagers, score, menu, foodMenu;
 class MainGame extends Phaser.Scene {
   constructor() {
     super("mainGame");
   }
-
   preload() {
     this.load.image("mainBg", "./../scenes/assets/main screen bg.jpg");
     this.load.image("mainBgBorder", "./../scenes/assets/mainScreenBorder.png");
@@ -18,24 +18,94 @@ class MainGame extends Phaser.Scene {
     this.load.image("potatoKwekKwek", "./../scenes/assets/potatoKwekKwek.png");
     this.load.image("kamoteLumpia", "./../scenes/assets/kamoteLumpia.png");
     this.load.image("angry", "./../scenes/assets/angry.png");
+    this.load.image(
+      "potatoKwekKwekMenu",
+      "./../scenes/assets/menuPotatoKwekKwek.png"
+    );
+    this.load.image(
+      "kamoteLumpiaMenu",
+      "./../scenes/assets/menuKamoteLumpia.png"
+    );
   }
 
   create() {
-    console.log(score)
+    score = 0;
     this.background = this.add.image(0, 0, "mainBg").setDepth(0);
     this.background.setOrigin(0, 0);
 
     this.backgroundBorder = this.add.image(0, 0, "mainBgBorder").setDepth(2);
     this.backgroundBorder.setOrigin(0, 0);
 
-    this.character = this.add.image(
-      this.game.renderer.width / 2,
-      this.game.renderer.height / 2 + 100,
-      "mainCharacter"
-    );
-    this.character.setScale(0.8, 0.8).setInteractive();
-    this.character.input.dropZone = true;
+    this.menuList = ["potatoKwekKwekMenu", "kamoteLumpiaMenu"];
+    foodMenu = this.add.group();
+    let initialPos = 62;
 
+    for (let i = 0; i < this.menuList.length; i++) {
+      let currentPos = initialPos;
+      menu = this.add
+        .image(currentPos, 60, this.menuList[i])
+        .setDepth(2)
+        .setScale(0.5);
+      initialPos += 90;
+      menu.setName(this.menuList[i]);
+      foodMenu.add(menu);
+      // menu.setInteractive();
+    }
+    foodMenu.children.each(function(food) {
+      food.setInteractive();
+      food.on(
+        "pointerdown",
+        function(pointer) {
+          let newFood = this.add
+            .image(pointer.x, pointer.y, food.name)
+            .setDepth(2)
+            .setScale(0.5)
+            .setInteractive({ draggable: true })
+            .setName(food.name)
+        },
+        this
+      );
+    }, this);
+
+    this.input.on("drag", function(pointer, obj, dragX, dragY) {
+      obj.setPosition(dragX, dragY);
+    });
+
+    this.input.on("drop", function(pointer, obj, dropZone) {
+      console.log("menu name " + obj.name);
+      console.log("dropzone name " + dropZone.name);
+      if (obj.name === dropZone.name) {
+        obj.x = dropZone.x;
+        obj.y = dropZone.y;
+        obj.input.enabled = false;
+        score += 10;
+        obj.destroy();
+        dropZone.destroy()
+        console.log("right " + score);
+      } else if (obj.name != dropZone.name) {
+        console.log("engggg");
+        obj.destroy()
+        dropZone.destroy()
+        score -= 10;
+        console.log("minus" + score);
+      }
+
+      // obj.destroy();
+    });
+
+    character = this.physics.add
+      .sprite(
+        this.game.renderer.width / 2,
+        this.game.renderer.height / 2 + 100,
+        "mainCharacter"
+      )
+      .setScale(0.8, 0.8)
+      .setInteractive();
+    // character.input.dropZone = true;
+    // this.input.on('gameobjectdown', function() {
+    //   this.score += 10
+    //   console.log(this.score)
+    // }, this)
     this.anims.create({
       key: "villager1",
       frames: this.anims.generateFrameNumbers("villagerOne", {
@@ -57,41 +127,45 @@ class MainGame extends Phaser.Scene {
       repeat: -1
     });
 
-    this.villagers = this.add.group();
+    villagers = this.add.group();
+    foodOrders = this.add.group();
+
     this.time.addEvent({
-      delay: 10000,
+      delay: Phaser.Math.Between(5000, 15000),
       callback: function() {
         let villager = new VillagerFromRight(
           this,
           this.game.renderer.width,
           this.game.renderer.height / 2 + 180
         );
-        villager.setDepth(1).setInteractive()
-        this.villagers.add(villager);
+        villager.setDepth(1).setInteractive();
+        villagers.add(villager);
       },
       callbackScope: this,
       loop: true
     });
 
     this.time.addEvent({
-      delay: 5000,
+      delay: Phaser.Math.Between(5000, 15000),
       callback: function() {
         let villager = new VillagerFromLeft(
           this,
           0,
           this.game.renderer.height / 2 + 180
         );
-        villager.flipX = true
-        villager.setDepth(1).setInteractive()
-        this.villagers.add(villager);
+        villager.flipX = true;
+        villager.setDepth(1).setInteractive();
+        villagers.add(villager);
       },
       callbackScope: this,
       loop: true
     });
+
+    // this.physics.add.collider(foodOrders, character, this.collides, null, this);
   }
 
- 
-
+  // collides(food, character) {
+  //   score += 10
+  //   console.log(score);
+  // }
 }
-
-
